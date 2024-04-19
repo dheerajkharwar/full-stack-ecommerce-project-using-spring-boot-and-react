06a679cba6da7e3b3c806894ba16e5d2cb2ca2c5
dheerajkharwar <dheerajkharwar9@gmail.com>
2024-04-19 11:30:31 +0530

    commit commit

--- react-ui/src/customer/components/carousel/HomeCarousel.jsx
+++ react-ui/src/customer/components/carousel/HomeCarousel.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import { homeCarouselData } from './HomeCarouselData';
 
-const items = [
-    <div className="item" data-value="1">1</div>,
-    <div className="item" data-value="2">2</div>,
-    <div className="item" data-value="2">2</div>,
-];
+const items = homeCarouselData.map((item)=><img className='cursor-pointer' role='presentation' src={item.image}/>)
 
 const HomeCarousel = () => (
     <AliceCarousel
